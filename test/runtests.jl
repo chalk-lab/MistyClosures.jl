31659406f1bc13dcd4dfacea57448c798baa561a
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-08-03 23:39:55 +0100

    compat fix for Julia 1.12 (#12)
    * Update MistyClosures.jl
    
    * Update MistyClosures.jl
    
    * Update CI.yml
    
    * Update Project.toml
    
    * Update MistyClosures.jl
    
    * Update MistyClosures.jl
    
    * Update CI.yml
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update MistyClosures.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update MistyClosures.jl
    
    * Update runtests.jl
    
    * Update test/runtests.jl
    
    * Update test/runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update MistyClosures.jl
    
    * Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,13 @@ struct Foo
     x::Float64
 end
 
-(f::Foo)(y) = f.x * y
+# We wrap `Foo` inside `Tuple{Foo}` to satisfy opaque closure assumption 
+# that its first argument has to be a `Tuple` storing captured variables
+(f::Tuple{Foo})(y) = f[1].x * y
 
 @testset "MistyClosures.jl" begin
-    ir = Base.code_ircode_by_type(Tuple{typeof(sin), Float64})[1][1]
+    ir = Base.code_ircode_by_type(Tuple{typeof(sin), Float64}) |> only |> first
+    ir.argtypes[1] = Tuple{}
 
     # Recommended constructor.
     mc = MistyClosure(ir; do_compile=true)
@@ -20,13 +23,13 @@ end
     @test @inferred(mc_default(5.0) == sin(5.0))
 
     # Recommended constructor with env.
-    ir_foo = Base.code_ircode_by_type(Tuple{Foo, Float64})[1][1]
-    mc_with_env = MistyClosure(ir_foo, 5.0; do_compile=true)
-    @test @inferred(mc_with_env(4.0)) == Foo(5.0)(4.0)
+    ir_foo = Base.code_ircode_by_type(Tuple{Tuple{Foo}, Float64}) |> only |> first
+    mc_with_env = MistyClosure(ir_foo, Foo(5.0); do_compile=true)
+    @test @inferred(mc_with_env(4.0)) == (Foo(5.0),)(4.0)
 
     # Default constructor with env.
-    mc_env_default = MistyClosure(OpaqueClosure(ir_foo, 4.0; do_compile=true), Ref(ir_foo))
-    @test @inferred(mc_env_default(5.0) == Foo(5.0)(4.0))
+    mc_env_default = MistyClosure(OpaqueClosure(ir_foo, Foo(5.0); do_compile=true), Ref(ir_foo))
+    @test @inferred(mc_env_default(4.0)) == (Foo(5.0),)(4.0)
 
     # deepcopy
     @test deepcopy(mc) isa typeof(mc)
@@ -34,5 +37,9 @@ end
     # printing -- we shouldn't see the IRCode, because it's often quite a lot.
     io = IOBuffer()
     show(io, mc)
-    @test String(take!(io)) == "MistyClosure (::Float64)::Float64->◌"
+    @static if VERSION >= v"1.12-"
+        @test String(take!(io)) == "MistyClosure (::Float64)->◌::Float64"
+    else
+        @test String(take!(io)) == "MistyClosure (::Float64)::Float64->◌"
+    end
 end
